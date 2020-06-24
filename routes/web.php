3d3e3133480055e5faacde619cdc00035a0144f3
Jhonathan Alfonso <salajhon69@gmail.com>
2020-06-23 21:33:56 -0500

    feat(admin html layout): admLTE was implemented

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/','home')->name('home');
-
-
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
+Route::view('/','home')->name('home');
+
+Route::get('/admin', 'AdminController@main')->name('admin.dashboard');
+
+Route::group([
+    'prefix' => 'admin',
+    'namespace' => 'Admin',
+    'middleware' => 'auth'],
+    function () {
+        //Route::get('/', 'AdminController@main')->name('admin.dashboard');
+});
+
+
+
+
