54a7cceb13bee10cd30f1fb7add13545133573f9
Jhonathan Alfonso <salajhon69@gmail.com>
2020-07-26 10:22:49 -0500

    chore(product seeder): product seeder was implemented with tags relation

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Tag;
 use App\Product;
 use Illuminate\Database\Seeder;
 
@@ -13,7 +14,22 @@ class ProductsTableSeeder extends Seeder
     public function run()
     {
         Product::truncate();
+        Tag::truncate();
 
-        factory(Product::class, 4)->create();
+
+        $product1 = factory(Product::class)->create();
+        $product1->tags()->attach(Tag::create(['name' => 'Sin etiqueta']));
+
+        $product2 = factory(Product::class)->create();
+        $product2->tags()->attach(Tag::create(['name' => 'Mujer']));
+
+        $product3 = factory(Product::class)->create();
+        $product3->tags()->attach(Tag::create(['name' => 'Hombre']));
+
+        $product4 = factory(Product::class)->create();
+        $product4->tags()->attach(Tag::create(['name' => 'Ropa']));
+
+        $product5 = factory(Product::class)->create();
+        $product5->tags()->attach(Tag::create(['name' => 'Zapatos']));
     }
 }
