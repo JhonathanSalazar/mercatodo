d7a8211e5b08601aedde3554f275cf813e6103ef
Jhonathan Alfonso <salajhon69@gmail.com>
2020-10-22 20:25:58 -0500

    fix: that is a test

--- app/Http/Controllers/Admin/UsersController.php
+++ app/Http/Controllers/Admin/UsersController.php
@@ -39,6 +39,8 @@ class UsersController extends Controller
                 ->get();
         });
 
+
+
         Log::info('admin.users.index', ['user', auth()->id()]);
 
         return view('admin.users.index', compact('users'));
